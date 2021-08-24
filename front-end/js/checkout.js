563a0effebba428cdcf71fc163798c636bb4f9ff
Jo-na-than <jonathan.palacios@orange.fr>
2021-08-24 03:01:31 +0200

    correction

--- front-end/js/checkout.js
+++ front-end/js/checkout.js
@@ -22,3 +22,18 @@ function displayOrder(orderId) { // Fonction qui affiche la commande
     }
     localStorage.removeItem("order"); // Supprime la commande passée du localStorage
 }
+
+function getTotalPrice() { // Fonction qui addictionne le coût total de la commande
+    let panier = JSON.parse(localStorage.getItem("panier")); // Stock les article du panier
+    let totalPanier = 0; // Initialise le total à 0
+    for (camera of panier) { // Lire le tableau des articles du panier
+        let quantity = parseFloat(camera.quantity); // Stock la quantité de l'article 
+        let price = parseFloat(camera.price); // Stock le prix de l'article
+        totalPanier += quantity * price; // Calcul le total du panier
+    }
+    return totalPanier; // Retourne le total du panier
+}
+
+function deleteCart() { // Fonction qui supprime les articles du panier
+    localStorage.removeItem("panier"); // Supprime les articles du panier
+}
